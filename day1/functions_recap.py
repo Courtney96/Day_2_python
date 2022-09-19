9e3e09ba1abc9678fd7bb66f1d6d861792ce493d
Courtney96 <bull_courtney@yahoo.com>
2022-09-19 11:42:23 +0100

    finding maximum number in a list

--- day1/functions_recap.py
+++ day1/functions_recap.py
@@ -1,20 +1,33 @@
 import sys
 
 
-def calculate_geometric_series(a, r, n=10):
-    """Calculate the sum of a geometric series"""
-    if r == 1:
-        return a * (n + 1)
-    return a * (1 - r ** (n + 1)) / (1 - r)
+# def calculate_geometric_series(a, r, n=10):
+#     """Calculate the sum of a geometric series"""
+#     if r == 1:
+#         return a * (n + 1)
+#     return a * (1 - r ** (n + 1)) / (1 - r)
+#
+#
+# def main():
+#     a = int(input("a: "))
+#     r = float(input("r: "))
+#     n = int(input("n: "))
+#     print(f"s_n = {calculate_geometric_series(a, r, n)}")
+#     return 0
+#
+#
+# if __name__ == "__main__":
+#     sys.exit(main())
+
+def get_max(a_list):
+    maximum = None
+    first = False
+    for value in a_list:
+        if not first:
+            maximum = value
+            first = True
+        if value > maximum:
+            maximum = value
+    return maximum
 
 
-def main():
-    a = int(input("a: "))
-    r = float(input("r: "))
-    n = int(input("n: "))
-    print(f"s_n = {calculate_geometric_series(a, r, n)}")
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())
